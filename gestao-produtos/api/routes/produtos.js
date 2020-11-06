326b91d3b33d65335f0344e5024964e013071ead
Alex Miller <alexmillerc@gmail.com>
2020-11-05 21:27:08 -0300

    Estrutura CRUD gestao-produtos alterada

---
+++ gestao-produtos/api/routes/produtos.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+  const controller = app.controllers.produtos;
+
+  app.route('/api/v1/produtos')
+    .get(controller.listProdutos)
+    .post(controller.saveProdutos);
+
+  app.route('/api/v1/produtos/:id')
+    .delete(controller.removeProdutos)
+    .put(controller.updateProdutos);
+}
