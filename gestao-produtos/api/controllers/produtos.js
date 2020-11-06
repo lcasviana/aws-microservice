7a03a56a7c723b916b1155acbd4e82198c458072
Alex Miller <alexmillerc@gmail.com>
2020-11-05 22:00:35 -0300

    Validação CRUD gestao-produto

--- gestao-produtos/api/controllers/produtos.js
+++ gestao-produtos/api/controllers/produtos.js
@@ -13,72 +13,88 @@ module.exports = app => {
 
   controller.saveProdutos = (req, res) => {
 
-    produtosMock.data.push({
-      id: uuidv4(),
-      nome: req.body.nome,
-      descricao: req.body.descricao,
-      setor: req.body.setor,
-      precoReais: req.body.precoReais,
-    });
+    if (!req.body.nome || typeof req.body.nome !== 'string' || !req.body.nome.trim()
+      || !req.body.descricao || typeof req.body.descricao !== 'string' || !req.body.descricao.trim()
+      || !req.body.setor || typeof req.body.setor !== 'string' || !req.body.setor.trim()
+      || !req.body.precoReais || typeof req.body.precoReais !== 'number' || Number.isNaN(req.body.precoReais) || req.body.precoReais < 0) 
+      
+       return res.status(400).json({ message: 'Cadastro inválido'});
 
-    res.status(201).json(produtosMock);
-  };
-
-  controller.removeProdutos = (req, res) => {
-    const {
-      id,
-    } = req.params;
-
-    const foundProdutosIndex = produtosMock.data.findIndex(produtos => produtos.id === id);
-
-    if (foundProdutosIndex === -1) {
-      res.status(404).json({
-        message: 'Produto não encontrado na base.',
-        success: false,
-        produtos: produtosMock,
-      });
-    } else {
-      produtosMock.data.splice(foundProdutosIndex, 1);
-      res.status(200).json({
-        message: 'Produto encontrado e deletado com sucesso!',
-        success: true,
-        produtos: produtosMock,
-      });
-    }
-  };
-
-  controller.updateProdutos = (req, res) => {
-    const {
-      id,
-    } = req.params;
-
-    const foundProdutosIndex = produtosMock.data.findIndex(produtos => produtos.id === id);
-
-    if (foundProdutosIndex === -1) {
-      res.status(404).json({
-        message: 'Produtos não encontrado na base.',
-        success: false,
-        produtos: produtosMock,
-      });
-    } else {
-      const newProdutos = {
-        id: id,
+      produtosMock.data.push({
+        id: uuidv4(),
         nome: req.body.nome,
         descricao: req.body.descricao,
         setor: req.body.setor,
         precoReais: req.body.precoReais,
-        createdAt: new Date()
-      };
+    });
 
-      produtosMock.data.splice(foundProdutosIndex, 1, newProdutos);
+      res.status(201).json(produtosMock);
+    };
 
-      res.status(200).json({
-        message: 'Produto encontrado e atualizado com sucesso!',
-        success: true,
-        produtos: produtosMock,
-      });
-    }
+
+controller.removeProdutos = (req, res) => {
+  const {
+    id,
+  } = req.params;
+
+  const foundProdutosIndex = produtosMock.data.findIndex(produtos => produtos.id === id);
+
+  if (foundProdutosIndex === -1) {
+    res.status(404).json({
+      message: 'Produto não encontrado na base.',
+      success: false,
+      produtos: produtosMock,
+    });
+  } else {
+    produtosMock.data.splice(foundProdutosIndex, 1);
+    res.status(200).json({
+      message: 'Produto encontrado e deletado com sucesso!',
+      success: true,
+      produtos: produtosMock,
+    });
   }
+};
 
-  return controller;
+controller.updateProdutos = (req, res) => {
+
+  if (!req.body.nome || typeof req.body.nome !== 'string' || !req.body.nome.trim()
+  || !req.body.descricao || typeof req.body.descricao !== 'string' || !req.body.descricao.trim()
+  || !req.body.setor || typeof req.body.setor !== 'string' || !req.body.setor.trim()
+  || !req.body.precoReais || typeof req.body.precoReais !== 'number' || Number.isNaN(req.body.precoReais) || req.body.precoReais < 0) 
+
+   return res.status(400).json({ message: 'Cadastro inválido'});
+
+  const {
+    id,
+  } = req.params;
+
+  const foundProdutosIndex = produtosMock.data.findIndex(produtos => produtos.id === id);
+
+  if (foundProdutosIndex === -1) {
+    res.status(404).json({
+      message: 'Produtos não encontrado na base.',
+      success: false,
+      produtos: produtosMock,
+    });
+  } else {
+    const newProdutos = {
+      id: id,
+      nome: req.body.nome,
+      descricao: req.body.descricao,
+      setor: req.body.setor,
+      precoReais: req.body.precoReais,
+      createdAt: new Date()
+    };
+
+    produtosMock.data.splice(foundProdutosIndex, 1, newProdutos);
+
+    res.status(200).json({
+      message: 'Produto encontrado e atualizado com sucesso!',
+      success: true,
+      produtos: produtosMock,
+    });
+  }
+}
+
+return controller;
 }
